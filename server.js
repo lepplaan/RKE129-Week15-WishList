c273827219bebfe0777ee31cbdfe421ab0c59884
Toomas Lepplaan <lepplaan@gmail.com>
2024-12-17 14:49:55 +0200

    Wishlist project

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const Wish = require('./model/wish');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    Wish.fetchAllWishes(wishesFromFile => {
+        console.log(wishesFromFile);
+        res.render('index', {myWishes: wishesFromFile});
+    });
+});
+
+app.post('/wish', (req, res) => {
+    let userData = req.body.userWish;
+    let newWish = new Wish(userData);
+    newWish.saveWish();
+    res.redirect('/');
+});
+
+const port = 5000
+
+app.listen(port, () => {
+    console.log(`Server is running ${port}`);
+})
